68b114fb634d4ec13f7f637f0850e9023574cc89
Héctor Garcia <hgarciaperis@gmail.com>
2020-12-05 18:18:20 +0100

    Clase StartView

---
+++ mastermind/src/main/java/es/escuelaIt/masterProgramacion/hector/GameView.java
@@ -0,0 +1,11 @@
+package es.escuelaIt.masterProgramacion.hector;
+
+public class GameView {
+
+	public GameView(Game game) {
+	}
+
+	public void write() {
+	}
+
+}
--- mastermind/src/main/java/es/escuelaIt/masterProgramacion/hector/StartView.java
+++ mastermind/src/main/java/es/escuelaIt/masterProgramacion/hector/StartView.java
@@ -1,11 +1,19 @@
 package es.escuelaIt.masterProgramacion.hector;
 
+import es.escuelaIt.masterProgramacion.hector.utils.Console;
+
 public class StartView {
 
+    private GameView gameView;
+
 	public StartView(Game game) {
+        this.gameView = new GameView(game);
 	}
 
 	public void interact() {
+        Console console = Console.getInstance();
+        console.writeln("-----------------MASTERMIND-----------------");
+        this.gameView.write();
 	}
 
 }
