7b92f21b472f7a8d9344c6e1677da55300b37a2a
Héctor Garcia <hgarciaperis@gmail.com>
2020-12-05 17:25:55 +0100

    Clase Mastermind

---
+++ mastermind/src/main/java/es/escuelaIt/masterProgramacion/hector/Game.java
@@ -0,0 +1,5 @@
+package es.escuelaIt.masterProgramacion.hector;
+
+public class Game {
+
+}
---
+++ mastermind/src/main/java/es/escuelaIt/masterProgramacion/hector/View.java
@@ -0,0 +1,11 @@
+package es.escuelaIt.masterProgramacion.hector;
+
+public class View {
+
+	public View(Game game) {
+	}
+
+	public void interact() {
+	}
+
+}
